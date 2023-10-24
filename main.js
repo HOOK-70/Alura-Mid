2095144dcd059cabc9b721a7a7635820312e475d
HOOK-70 <126995889+HOOK-70@users.noreply.github.com>
2023-10-23 21:33:14 -0300

    CriandoMain

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("Bora programar galera");
+function somTocagrilo (){
+    document.querySelector("#som_tecla_grilo").play();
+}
+
+document.querySelector(".tecla_grilo").onclick = somTocagrilo;
+
+const ListaDeTeclas = document.querySelectorAll(".tecla");
+listaDeTeclas[4].onclick = tocaSomGrilo;
